137712e914dd8635aed42cfba53bc05a0423d420
siddhesh-grl <siddhesh-grl@users.noreply.github.com>
2022-01-06 09:44:20 +0000

    deploy: a41fc11dc27bbc6c989ccfc92a78ddd684e6c1ad

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['activescanresult_668',['ActiveScanResult',['../d2/da8/structotbr_1_1DBus_1_1ActiveScanResult.html',1,'otbr::DBus']]],
-  ['application_669',['Application',['../d5/d01/structotbr_1_1Application.html',1,'otbr']]],
-  ['avahitimeout_670',['AvahiTimeout',['../db/dfa/structAvahiTimeout.html',1,'']]],
-  ['avahiwatch_671',['AvahiWatch',['../de/d3f/structAvahiWatch.html',1,'']]]
+  ['activescanresult_659',['ActiveScanResult',['../d2/da8/structotbr_1_1DBus_1_1ActiveScanResult.html',1,'otbr::DBus']]],
+  ['avahitimeout_660',['AvahiTimeout',['../db/dfa/structAvahiTimeout.html',1,'']]],
+  ['avahiwatch_661',['AvahiWatch',['../de/d3f/structAvahiWatch.html',1,'']]]
 ];
--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['neighborinfo_732',['NeighborInfo',['../d2/d47/structotbr_1_1DBus_1_1NeighborInfo.html',1,'otbr::DBus']]],
-  ['nodeinfo_733',['NodeInfo',['../d6/dbf/structotbr_1_1rest_1_1NodeInfo.html',1,'otbr::rest']]],
-  ['noncopyable_734',['NonCopyable',['../dc/d3c/classNonCopyable.html',1,'']]]
+  ['neighborinfo_722',['NeighborInfo',['../d2/d47/structotbr_1_1DBus_1_1NeighborInfo.html',1,'otbr::DBus']]],
+  ['nodeinfo_723',['NodeInfo',['../d6/dbf/structotbr_1_1rest_1_1NodeInfo.html',1,'otbr::rest']]]
 ];
